06fe16c6958a43dd158aa8bb1dcd7db49a4dc8c3
ngDeveloper <askaraliyevsunnatillo@gmail.com>
2024-08-25 11:57:57 +0500

    modals

--- src/app/core/services/ideas.service.ts
+++ src/app/core/services/ideas.service.ts
@@ -1,9 +1,15 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
 import { IIdea } from '@core/models/IIdea';
 import { IResponse } from '@core/models/IResponse';
+
+interface Idea {
+  title: string;
+  description: string;
+  categoryId: number;
+}
 
 @Injectable({ providedIn: 'root' })
 export class IdeasService {
@@ -11,5 +17,22 @@ export class IdeasService {
 
   getAllIdeas(): Observable<IResponse<IIdea[]>> {
     return this.httpClient$.get<IResponse<IIdea[]>>(environment.api + 'ideas/get-all');
+  }
+  addIdeas(body: any) {
+    const json = localStorage.getItem('user');
+    const user = JSON.parse(json!);
+    const header = new HttpHeaders({
+      // 'Content-Type': 'multipart/form-data',
+      Authorization: 'Bearer ' + user.token,
+    });
+    const data = body as Idea;
+
+    const formData = Object.entries(data).reduce((fd, [key, val]) => {
+      fd.append(key, val);
+      return fd;
+    }, new FormData());
+    console.log(body);
+
+    return this.httpClient$.post(environment.api + 'ideas/create', formData, { headers: header });
   }
 }
--- src/app/features/my-ideas/my-ideas.component.ts
+++ src/app/features/my-ideas/my-ideas.component.ts
@@ -1,16 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTab, MatTabGroup } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ModalDialogComponent } from '@core/components/modal-dialog/modal-dialog.component';
 import { NavbarComponent } from '@core/components/navbar/navbar.component';
 import { IdeaComponent } from '@shared/components/idea/idea.component';
 
 @Component({
   standalone: true,
   selector: 'app-my-ideas',
-  imports: [NavbarComponent, MatTabGroup, MatTab, MatButtonModule, IdeaComponent],
+  imports: [NavbarComponent, MatTabGroup, MatTab, MatButtonModule, IdeaComponent, MatToolbarModule, MatIconModule],
   templateUrl: './my-ideas.component.html',
   styles: ``,
 })
 export default class MyIdeasComponent {
-  ideas = [true, false, true, false, true, false];
+  ideas = [true, false];
+  dialog = inject(MatDialog);
+  openModal() {
+    this.dialog.open(ModalDialogComponent, {
+      data: {
+        clickedPlace: 'myideas',
+      },
+    });
+  }
 }
